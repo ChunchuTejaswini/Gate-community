7c55ef1d4621a8a0d5df4d1b11ebdcacdaf8f378
Tejaswini Chunchu <102965650+ChunchuTejaswini@users.noreply.github.com>
2026-06-14 21:07:54 +0530

    Update GatedCommunityApplication.java

--- src/main/java/com/controller/gatedcommunity/GatedCommunityApplication.java
+++ src/main/java/com/controller/gatedcommunity/GatedCommunityApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatedCommunityApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("MYSQLHOST = " + System.getenv("MYSQLHOST"));
+        System.out.println("MYSQLPORT = " + System.getenv("MYSQLPORT"));
+        System.out.println("MYSQLDATABASE = " + System.getenv("MYSQLDATABASE"));
+        System.out.println("MYSQLUSER = " + System.getenv("MYSQLUSER"));
+
         SpringApplication.run(GatedCommunityApplication.class, args);
     }
 
